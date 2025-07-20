6b7ca2ee90259c89475441e899517802e7b7013e
Alvorada9999 <kenedyhenrique.buenosilva@gmail.com>
2025-07-19 22:03:00 -0300

    lvim update

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,12 +1,3 @@
-require('lspconfig').astro.setup({
-  init_options = {
-    typescript = {
-      -- Depends on nvm installation with current nodejs global typescript installation
-      tsdk = vim.fs.normalize('$NVM_BIN/../lib/node_modules/typescript/lib')
-    }
-  },
-})
-
 local dap = require("dap")
 
 dap.adapters.gdb = {
@@ -24,11 +15,17 @@ local cCPPConfigs = {
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
     cwd = "${workspaceFolder}",
+    console = "integratedTerminal",
+    args = function()
+      local input = vim.fn.input('Program arguments: ')
+      return vim.fn.split(input, " ", true)
+    end,
   },
   {
     name = "Select and attach to process",
     type = "gdb",
     request = "attach",
+    stopAtBeginningOfMainSubprogram = true,
     -- program = function()
     --    return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     -- end,
@@ -40,70 +37,78 @@ local cCPPConfigs = {
     pid = function()
        return tonumber(vim.fn.input('Pid: '))
     end,
-    cwd = '${workspaceFolder}'
+    cwd = '${workspaceFolder}',
+    console = "integratedTerminal",
   },
 }
 dap.configurations.c = cCPPConfigs;
 dap.configurations.cpp = cCPPConfigs;
 
--- Install node-debug2
---   mkdir -p ~/dev/microsoft
---   git clone https://github.com/microsoft/vscode-node-debug2.git ~/dev/microsoft/vscode-node-debug2
---   cd ~/dev/microsoft/vscode-node-debug2
---   npm install
---   NODE_OPTIONS=--no-experimental-fetch npm run build
-dap.adapters.node2 = {
-  type = 'executable',
-  command = 'node',
-  args = {os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js'},
+------------------------------------------------------------------------------------
+-- vscode-js-debug
+-- A DAP-compatible JavaScript debugger.
+
+-- vscode-js-debug contains 2 executables:
+
+--     dapDebugServer.js (recommended) available since v1.77.0. All examples below that use dapDebugServer require you to use this executable.
+--     vsDebugServer.js vscode specific (to make it work with nvim-dap you can use use nvim-dap-vscode-js but the above variant is recommended)
+
+-- A Manual setup using the dapDebugServer executable looks like this:
+
+--     Make sure you're using nvim-dap 0.6.0+
+
+--     Install vscode-js-debug (https://github.com/microsoft/vscode-js-debug):
+--         Download js-debug-dap-${version}.tar.gz from their releases page. You need at least version 1.77.0 (it might be not available yet in newer versions)
+--         Extract it to some folder via tar xvzf path/to/vscode-js-debug.tar.gz.
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    args = {vim.fn.expand("~") .. "/js-debug/src/dapDebugServer.js", "${port}"},
+  }
 }
 dap.configurations.javascript = {
   {
     name = 'Launch',
-    type = 'node2',
+    type = 'pwa-node',
     request = 'launch',
-    stopAtBeginningOfMainSubprogram = true,
-    -- program = '${file}',
     program = function()
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
-    -- program = "/home/snake/Discord-video-stream/examples/basic/dist/index.js",
     cwd = vim.fn.getcwd(),
-    sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-  {
-    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-    name = 'Attach to process',
-    type = 'node2',
-    request = 'attach',
-    processId = require'dap.utils'.pick_process,
+    stopOnEntry = true,
+    console = "integratedTerminal"
   },
 }
 dap.configurations.typescript = {
   {
     name = 'Launch',
-    type = 'node2',
+    type = 'pwa-node',
     request = 'launch',
-    -- program = '${file}',
     program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/dist/', 'file')
     end,
     cwd = vim.fn.getcwd(),
+    stopOnEntry = true,
     sourceMaps = true,
-    protocol = 'inspector',
-    console = 'integratedTerminal',
-  },
-  {
-    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-    name = 'Attach to process',
-    type = 'node2',
-    request = 'attach',
-    processId = require'dap.utils'.pick_process,
+    outFiles = { "${workspaceFolder}/dist/**/*.js", "!**/node_modules/**" },
+    console = "integratedTerminal"
   },
 }
+------------------------------------------------------------------------------------
 
+local function get_python()
+  local cwd = vim.fn.getcwd()
+  if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+    return cwd .. '/venv/bin/python'
+  elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+    return cwd .. '/.venv/bin/python'
+  else
+    return 'python3'
+  end
+end
 dap.adapters.python = function(cb, config)
   if config.request == 'attach' then
     ---@diagnostic disable-next-line: undefined-field
@@ -121,8 +126,8 @@ dap.adapters.python = function(cb, config)
   else
     cb({
       type = 'executable',
-      command = '/home/snake/.virtualenvs/debugpy/bin/python',
-      args = { '-m', 'debugpy.adapter' },
+      command = get_python(),
+      args = { '-Xfrozen_modules=off', '-m', 'debugpy.adapter' },
       options = {
         source_filetype = 'python',
       },
@@ -140,6 +145,9 @@ dap.configurations.python = {
     -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
     program = "${file}"; -- This configuration will launch the current file if used.
+    -- pythonPath = "python3";
+    console = "integratedTerminal";
+    stopOnEntry = true;
     pythonPath = function()
       -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
       -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
@@ -150,10 +158,20 @@ dap.configurations.python = {
       elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
         return cwd .. '/.venv/bin/python'
       else
-        return '/usr/bin/python'
+        return '/usr/bin/python3'
       end
     end;
   },
 }
 
--- lvim.transparent_window = true
+lvim.builtin.which_key.mappings["d"]["h"] = {
+  function() require("dap.ui.widgets").hover() end,
+  "Hover All Variables",
+}
+
+lvim.builtin.which_key.mappings["d"]["H"] = {
+  function() local widgets=require('dap.ui.widgets');widgets.centered_float(widgets.scopes) end,
+  "Hover Variable",
+}
+
+lvim.transparent_window = true
